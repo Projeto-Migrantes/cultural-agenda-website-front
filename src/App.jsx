9b6dbb1888bb799a5af36285df62306aff4cdd5e
Alissonbrito9919 <alisson.brito9919@gmail.com>
2025-05-27 22:37:11 -0300

    Atualiza o rodapé

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { ThemeProvider } from "styled-components";
-
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "@routes/routes";
 import theme from "@theme/theme";
 import GlobalStyle from "@theme/global";
-
+import Footer from "@components/Footer";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <><ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
         <AppRoutes />
       </Router>
-    </ThemeProvider>
-  );
+    </ThemeProvider><Footer /></>
+);
 }
 
 export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,13 +2,15 @@ import * as S from "./styles.js";
 import Hero from "../../components/Hero/index.jsx";
 import Navbar from "../../components/Navbar/index.jsx";
 
+
 function Home() {
   return (
     <S.Container>
        <Hero />
       <Navbar />
-
     </S.Container>
+  
+  
   );
 }
 
