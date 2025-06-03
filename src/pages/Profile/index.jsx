5dc247f16bc72adf270f9ba139c4d1ddb5861bd6
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-03 19:12:52 -0300

    feat(profile): add DeleteButton to profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "@components/Header/index.jsx";
 import * as S from "./styles.js";
+import { DeleteButton } from "@components/DeleteButton/index.jsx";
 
 function Profile() {
   return (
@@ -8,6 +9,7 @@ function Profile() {
       <S.Container>
         <S.Title>Página 2 de Profile </S.Title>
       </S.Container>
+      <DeleteButton/>
     </>
   );
 }
