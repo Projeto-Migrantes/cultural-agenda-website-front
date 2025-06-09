b4ae2f65a973e62f0921bdae6ae58b243b25a0bd
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-09 18:57:10 -0300

    refactor(checkbox): update component and adjust usage in profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { Header } from "@components/Header/index.jsx";
 import * as S from "./styles.js";
 import { TitleComponent } from "@components/TitleComponent/index.jsx";
 import { CheckBox } from "@components/Checkbox/index.jsx";
 
 function Profile() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleCheckboxChange(){
+    setIsChecked(!isChecked);
+  }
+
+
   return (
     <>
       <Header />
@@ -18,7 +26,7 @@ function Profile() {
           }
         />
       </S.Container>
-      <CheckBox>Aceito o <strong>Termo de Uso</strong> e <strong>Política de Privacidade</strong></CheckBox>
+      <CheckBox isChecked={isChecked} handleOnChange={handleCheckboxChange}>Aceito o <strong>Termo de Uso</strong> e <strong>Política de Privacidade</strong></CheckBox>
     </>
   );
 }
